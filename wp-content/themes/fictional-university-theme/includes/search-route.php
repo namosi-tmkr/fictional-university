da4e012296b997f89ea8b957152a3c1dab2ed8e9
Namosi <namosi@grafioffshore.com>
2017-12-20 11:55:05 +0545

    managed the search for campuses related with particular program.. finished the search overlay job

--- wp-content/themes/fictional-university-theme/includes/search-route.php
+++ wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -47,6 +47,17 @@ function universitySearchResults($data)
 		}
 
 		if(get_post_type() == 'program') {
+			$relatedCampuses = get_field('related_campus');
+
+			if($relatedCampuses) {
+				foreach ($relatedCampuses as $campus) {
+					array_push($results['campuses'], [
+						'title' 	=> get_the_title($campus),
+						'permalink' => get_the_permalink($campus)
+					]);
+				}
+			}
+
 			array_push($results['programs'], [
 			'id' 		=> get_the_ID(),
 			'title' 	=> get_the_title(),
@@ -94,7 +105,7 @@ function universitySearchResults($data)
 		}
 
 		$programRelationshipQuery = new WP_Query([
-			'post_type'  => 'professor',
+			'post_type'  => ['professor', 'event', ],
 			'meta_query' => $programsMetaQuery, 
 		]);
 
@@ -109,9 +120,27 @@ function universitySearchResults($data)
 				]);
 			}
 
+			if(get_post_type() == 'event') {
+				$eventDate = new DateTime(get_field('event_date'));
+				$description = null;
+				if(has_excerpt()) {
+					$description = get_the_excerpt();
+				} else {
+					$description = wp_trim_words(get_the_content(), 18);
+				}
+				array_push($results['events'], [
+					'title' 	=> get_the_title(),
+					'permalink'	=> get_the_permalink(),
+					'month'		=> $eventDate->format('M'),
+					'day'		=> $eventDate->format('d'),
+					'description' => $description,
+				]);
+			}
+
 		}
 
-		$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+		$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR)); //removes dublicate matched data due to two queries... i.e. if both queries find the same data then this will remove dublication of that data
+		$results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
 	}
 	
 
