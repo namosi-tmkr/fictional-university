d4f1cb96a29dcd762a2bbce14c9cc5841fb1344f
Namosi <namosi@grafioffshore.com>
2017-12-20 15:30:16 +0545

    customized the login page

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -165,3 +165,29 @@ function noSubsAdminBar()
 		show_admin_bar(false);
 	}
 }
+
+
+//Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() 
+{
+	return esc_url(site_url('/'));
+}
+
+
+//customize login screen logo
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS()
+{
+	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+	wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle()
+{
+	return get_bloginfo('name');
+}
