bd39f989f8e27cf5c047dd017ad598a826ea879a
Namosi <namosi@grafioffshore.com>
2017-12-21 14:09:51 +0545

    created Like.js file to like and dislike a professor

--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -10344,6 +10344,8 @@ var _Search = _interopRequireDefault(__webpack_require__(6));
 
 var _MyNotes = _interopRequireDefault(__webpack_require__(7));
 
+var _Like = _interopRequireDefault(__webpack_require__(8));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // 3rd party packages from NPM
@@ -10354,6 +10356,7 @@ var heroSlider = new _HeroSlider.default();
 var googleMap = new _GoogleMap.default();
 var search = new _Search.default();
 var mynotes = new _MyNotes.default();
+var like = new _Like.default();
 
 /***/ }),
 /* 2 */
@@ -13949,6 +13952,72 @@ function () {
 }();
 
 var _default = MyNotes;
+exports.default = _default;
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _jquery = _interopRequireDefault(__webpack_require__(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Like =
+/*#__PURE__*/
+function () {
+  function Like() {
+    _classCallCheck(this, Like);
+
+    this.events();
+  }
+
+  _createClass(Like, [{
+    key: "events",
+    value: function events() {
+      (0, _jquery.default)(".like-box").on("click", this.ourClickDispatcher.bind(this));
+    } //methods
+
+  }, {
+    key: "ourClickDispatcher",
+    value: function ourClickDispatcher(e) {
+      var currentLikeBox = (0, _jquery.default)(e.target.closest(".like-box"));
+
+      if (currentLikeBox.data('exists') == 'yes') {
+        this.deleteLike();
+      } else {
+        this.createLike();
+      }
+    }
+  }, {
+    key: "createLike",
+    value: function createLike() {
+      alert('create test message');
+    }
+  }, {
+    key: "deleteLike",
+    value: function deleteLike() {
+      alert('delete test message');
+    }
+  }]);
+
+  return Like;
+}();
+
+var _default = Like;
 exports.default = _default;
 
 /***/ })
